3a9ae55b35db163c076c60c25f1402de5abbec3e
Kycool13 <mattingly.luke@yahoo.com>
2024-02-25 16:26:49 -0700

    computed properties

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -8,10 +8,16 @@
     <h3>{{ counterData.title }}</h3>
 
     <div>
-      <button @click="decreaseCounter" class="btn">-</button>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
       <span class="counter">{{ counterData.count }}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
     </div>
+
+    <p>
+      This counter is {{ oddOrEvent }}
+    </p>
 
     <div class = "edit">
       <h4> Edit counter title:</h4>
@@ -22,7 +28,7 @@
 </template>
 
 <script setup>
-import {reactive} from 'vue'
+import {reactive, computed} from 'vue'
 
 const appTitle = 'My Ok Counter App'
 
@@ -30,12 +36,17 @@ const counterData = reactive({
   count: 0,
   title: 'My Counter'
 })
+
+const oddOrEvent = computed(() =>{
+  if (counterData.count %2 ===0) return 'even'
+  return 'odd'
+})
     
-const increaseCounter = () =>{
-  counterData.count ++
+const increaseCounter = (amount, e) =>{
+  counterData.count += amount
 }
-const decreaseCounter = ()=>{
-  counterData.count --
+const decreaseCounter = amount=>{
+  counterData.count -=amount
 }
 
 
