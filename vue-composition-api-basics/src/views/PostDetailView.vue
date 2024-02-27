29739f0567762eb53f017ffce9d43b12153604b1
lmattingly13 <lmattingly@redventures.com>
2024-02-26 17:09:23 -0700

    new posts detail view

---
+++ vue-composition-api-basics/src/views/PostDetailView.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <div class = "post-detail">
+    <h1> This is a post page</h1>
+    <textarea v-autofocus />
+  </div>
+</template>
+
+<script setup>  
+
+
+</script>
+
+
---
+++ vue-composition-api-basics/src/views/PostsView.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div class = "posts">
+    <h1> Posts</h1>
+
+    <ul>
+      <li>
+        <RouterLink to= "/postDetail/id1"> Post1</RouterLink>
+      </li>
+      <li>
+        <RouterLink to= "/postDetail/id2"> Post2</RouterLink>
+      </li>
+      <li>
+        <RouterLink to= "/postDetail/id3"> Post3</RouterLink>
+      </li>
+    </ul>
+
+    <textarea v-autofocus />
+  </div>
+</template>
+
+<script setup>  
+
+import {vAutofocus} from '@/directives/vAutofocus'
+
+</script>
+
+
+<style scoped>
+
+ul {
+  margin-bottom: 30px;
+}
+</style>
