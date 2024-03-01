49f9c7f85cb1c12b942fa51b8221a1d1a243efaa
lmattingly13 <lmattingly@redventures.com>
2024-03-01 14:47:34 -0700

    create modal, remove lazy loading

---
+++ vue-composition-api-basics/src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+    <Teleport to=".modal-container">
+        <div class="modal">
+        <h1>This is a modal</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla possimus similique corporis pariatur, minus a voluptate aspernatur suscipit eligendi repellendus inventore commodi quaerat nisi voluptatum sed magni placeat debitis est.</p>
+        <button>Hide Modal</button>
+    </div>
+    </Teleport>
+</template>
+
+
+<style>
+.modal{
+    background:beige;
+    padding:10px;
+    position:absolute;
+    left: 0;
+    top:0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+
+</style>
--- vue-composition-api-basics/src/views/ModalsView.vue
+++ vue-composition-api-basics/src/views/ModalsView.vue
@@ -2,36 +2,28 @@
     <div class="modals">
         <h1> Modals</h1>
         <button @click="showModal=true">Show Modal</button>
-        <Teleport to=".modal-container">
-            <div 
-            v-if = "showModal" 
-            class="modal"
-            >
-            <h1>This is a modal</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla possimus similique corporis pariatur, minus a voluptate aspernatur suscipit eligendi repellendus inventore commodi quaerat nisi voluptatum sed magni placeat debitis est.</p>
-            <button @click="showModal=false">Hide Modal</button>
-        </div>
-        </Teleport>
+        <Modal v-if="showModal"></Modal>
     </div>
 </template>
 
 <script setup>
 import {ref} from 'vue'
+import Modal from '@/components/Modal.vue'
+
 /*modals*/
 
 const showModal = ref(false)
 </script>
 
-<style>
-.modal{
-    background:beige;
-    padding:10px;
-    position:absolute;
-    left: 0;
-    top:0;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-}
+<!--
 
-</style>
+    <script>
+import Modal from '@/components/Modal.vue'
+
+export default {
+    components: {
+        Modal
+    }
+}  
+</script>
+-->
